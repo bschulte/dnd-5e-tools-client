07f1098bfdbb8241e00c789554ed3f3bb80e7017
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2018-12-23 14:50:11 -0500

    background click and escape key close

--- src/components/core/Modal.tsx
+++ src/components/core/Modal.tsx
@@ -1,32 +1,68 @@
 import * as React from "react";
 import classNames from "classnames";
+import { HotKey } from "./";
 
 export interface IModalProps {
   isOpen: boolean;
   toggle: () => void;
+  escapeClose?: boolean;
+  backgroundClickClose?: boolean;
 }
 
-export default class Modal extends React.Component<IModalProps, any> {
+export class Modal extends React.Component<IModalProps, any> {
   public render() {
-    const { isOpen, toggle } = this.props;
+    const {
+      isOpen,
+      toggle,
+      escapeClose = true,
+      backgroundClickClose = true
+    } = this.props;
 
     return (
-      <div
-        className={classNames(
-          "fixed pin z-50 overflow-auto bg-smoke-light flex",
-          { "visible animated fadeIn": isOpen },
-          { invisible: !isOpen }
+      <React.Fragment>
+        {/* Close on escape key */}
+        {escapeClose && (
+          <HotKey
+            hotkey="escape"
+            onTrigger={() => {
+              if (isOpen) {
+                toggle();
+              }
+            }}
+          />
         )}
-      >
+
+        {/* Modal background */}
         <div
           className={classNames(
-            "relative align-top p-8 bg-white w-full max-w-md m-auto flex-col flex",
-            { "animated slideInUp": isOpen }
+            "animated faster fixed pin z-50 overflow-auto bg-smoke-light flex",
+            { "visible fadeIn": isOpen },
+            { fadeOut: !isOpen }
           )}
+          onClick={(e: React.MouseEvent) => {
+            // Close the modal on background click if specified via props
+            if (backgroundClickClose && isOpen) {
+              toggle();
+            }
+          }}
         >
-          Centered modal!
+          {/* Modal content */}
+          <div
+            className={classNames(
+              "animated faster relative p-8 bg-white w-full mx-auto mb-auto mt-16 max-w-md flex-col flex",
+              { slideInUp: isOpen },
+              { slideOutDown: !isOpen }
+            )}
+            /*
+              Stop propagation of clicks here so they don't go to the background
+              div which would close the modal if the backgroundCloseClick prop is specified
+            */
+            onClick={(e: React.MouseEvent) => e.stopPropagation()}
+          >
+            Centered modal!
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
