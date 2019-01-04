07768dd4568485f152a5d90d5e7ebd2b46958c31
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-01-03 23:58:17 -0500

    use react-spring for modal animations

--- src/components/core/ui/Modal.tsx
+++ src/components/core/ui/Modal.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import classNames from "classnames";
 
 import { HotKey } from "..";
+import { Transition } from "react-spring";
 
 export interface IModalProps {
   children: React.ReactNode;
@@ -40,33 +41,57 @@ export class Modal extends React.Component<IModalProps, any> {
         )}
 
         {/* Modal background */}
-        <div
-          className={classNames(
-            "animated fadeIn fixed pin z-50 overflow-auto bg-smoke flex"
-          )}
-          onClick={(e: React.MouseEvent) => {
-            // Close the modal on background click if specified via props
-            if (backgroundClickClose && isOpen) {
-              toggle();
-            }
-          }}
+        <Transition
+          items={isOpen}
+          from={{ opacity: 0 }}
+          enter={{ opacity: 1 }}
+          leave={{ opacity: 0 }}
         >
-          {/* Modal content */}
-          <div
-            className={classNames(
-              "animated faster relative p-8 bg-grey-darkest w-full mx-auto mb-auto mt-16 max-w-md flex-col flex",
-              { slideInUp: isOpen },
-              { slideOutDown: !isOpen }
-            )}
-            /*
-              Stop propagation of clicks here so they don't go to the background
-              div which would close the modal if the backgroundCloseClick prop is specified
-            */
-            onClick={(e: React.MouseEvent) => e.stopPropagation()}
-          >
-            {children}
-          </div>
-        </div>
+          {show =>
+            show &&
+            (animationProps => (
+              <div
+                className={classNames(
+                  "fixed pin z-50 overflow-auto bg-smoke flex"
+                )}
+                onClick={(e: React.MouseEvent) => {
+                  // Close the modal on background click if specified via props
+                  if (backgroundClickClose && isOpen) {
+                    toggle();
+                  }
+                }}
+                style={animationProps}
+              >
+                {/* Modal content */}
+                <Transition
+                  items={isOpen}
+                  from={{ transform: "translate3d(0,-40px,0)" }}
+                  enter={{ transform: "translate3d(0,0px,0)" }}
+                  leave={{ transform: "translate3d(0,-40px,0)" }}
+                >
+                  {show =>
+                    show &&
+                    (animationProps => (
+                      <div
+                        className={classNames(
+                          "relative p-8 bg-grey-darkest w-full mx-auto mb-auto mt-16 max-w-md flex-col flex"
+                        )}
+                        style={animationProps}
+                        /*
+                          Stop propagation of clicks here so they don't go to the background
+                          div which would close the modal if the backgroundCloseClick prop is specified
+                        */
+                        onClick={(e: React.MouseEvent) => e.stopPropagation()}
+                      >
+                        {children}
+                      </div>
+                    ))
+                  }
+                </Transition>
+              </div>
+            ))
+          }
+        </Transition>
       </React.Fragment>
     );
   }
