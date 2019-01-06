35247bbb958e6dc0a60bb9ef003cd59bc3c6bbf8
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-01-06 12:47:03 -0500

    perform query on load to see if user is still logged in

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,25 @@ import { client } from "./graphql/client";
 
 import DashboardPage from "./pages/DashboardPage";
 import LoginPage from "./pages/LoginPage";
-import { Navbar } from "./components/core";
 import AppNavbar from "./components/AppNavbar";
+import { getToken, login, logout } from "./util/auth";
+import { GET_USER } from "./graphql/queries";
 
 class App extends Component<any, any> {
+  componentDidMount = async () => {
+    // Perform an initial query on the users info. This is to check
+    // if the stored token is still valid (the user's session is still active)
+    const token = getToken();
+    if (token) {
+      const { data } = await client.query({ query: GET_USER });
+      if (data) {
+        login(token);
+      } else {
+        logout();
+      }
+    }
+  };
+
   render() {
     return (
       <div className="animated fadeIn">
