bca7a1f9618767974215496a0345484be8ef4ceb
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-01-06 12:47:21 -0500

    same as before

--- src/components/AppNavbar.tsx
+++ src/components/AppNavbar.tsx
@@ -1,13 +1,26 @@
 import * as React from "react";
+
+import dragonIcon from "../images/dragon_icon.png";
 import { Navbar, NavItem } from "./core";
+import { isLoggedIn } from "../util/auth";
 
 export interface IAppNavbarProps {}
 
 export default class AppNavbar extends React.Component<IAppNavbarProps, any> {
   public render() {
+    if (!isLoggedIn()) {
+      return null;
+    }
+
     return (
-      <Navbar title="DnD 5e Tools">
-        <NavItem to="/">Testing</NavItem>
+      <Navbar title="DnD 5e Tools" brandImg={dragonIcon}>
+        <div className="flex">
+          <NavItem to="/">Compendium</NavItem>
+          <NavItem to="/">Characters</NavItem>
+        </div>
+        <div className="flex">
+          <i className="far fa-sign-out text-xl cursor-pointer" />
+        </div>
       </Navbar>
     );
   }
