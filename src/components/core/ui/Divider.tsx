a840ea6eeaf46b4c02dfc30d03039a5a2fcb05be
Brian Schulte <bschulte@Brians-MacBook-Pro.local>
2019-01-06 12:47:10 -0500

    divider changes

--- src/components/core/ui/Divider.tsx
+++ src/components/core/ui/Divider.tsx
@@ -1,11 +1,21 @@
 import * as React from "react";
+import classNames from "classnames";
 
-export interface IHRProps {}
+export interface IHRProps {
+  vertical?: boolean;
+}
 
 export class Divider extends React.Component<IHRProps, any> {
   public render() {
+    const { vertical = false } = this.props;
+
     return (
-      <div className="text-white my-2 border-r-2 border-b-2 border-grey" />
+      <div
+        className={classNames("text-white border-r-2 border-b-2 border-grey", {
+          "flex height-full mx-2": vertical,
+          "my-2": !vertical
+        })}
+      />
     );
   }
 }
